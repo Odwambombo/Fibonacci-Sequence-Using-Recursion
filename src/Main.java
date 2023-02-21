31e65230de42db6d798bf098950e25e2aad42123
odwambombo <odwa.mbombo@gmail.com>
2023-02-21 12:07:39 +0200

    Fibonacci

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,31 @@
 import java.util.Scanner;
 
 public class Main {
+    public static int[] array;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please type your Fibonacci Index number and press Enter: ");
         int data = scanner.nextInt();
+        array = new int[data +1];
         System.out.println("Your Fibonacci number is "+ Fibonacci(data)+ " at index "+ data );
+        System.out.println();
+        System.out.println("The Fibonacci sequence from 0 to your index: ");
+        for(int i=0; i < data+1; i++){
+            System.out.print(Fibonacci(i)+" ");
+        }
     }
+
     public static int Fibonacci(int data){
-        int array[]=new int[data];
+
         if(data <= 1){
             return data;
         }
-
-        array[0]= 0; array[1]=1;
-        array[data-1] = Fibonacci(data-1) + Fibonacci(data-2);
-        return array[data-1];
+        if(array[data] != 0){
+            return array[data];
+        }
+        int value = Fibonacci(data-1) + Fibonacci(data-2);
+        array[data] = value;
+        return value;
     }
 
 }
